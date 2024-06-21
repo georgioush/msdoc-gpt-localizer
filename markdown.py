ef61ad4b4e4e950f00d4e66aec6081cd1995141e
Akira Shoji <georgioush@gmail.com>
2024-06-21 22:53:53 +0900

    added markdown class

---
+++ markdown.py
@@ -0,0 +1,11 @@
+class TokenizedMarkdown:
+    def __init__(self):
+        self.meta_info = None
+        self.tokenized_sections = []
+
+class Markdown:
+    def __init__(self, content):
+        self.content = content
+        self.translated_content = None
+        self.summary_content = None
+        self.tokenized_content = TokenizedMarkdown()
--- markdown_tokenizer.py
+++
@@ -1,9 +0,0 @@
-from markdown_handler import Markdown
-
-class MarkdownTokenizer:
-    def __init__(self, markdown: Markdown):
-        self.markdown = markdown
-        self.tokenized_content = None
-
-    def tokenize(self):
-        raise NotImplementedError("Subclasses should implement this method.")
