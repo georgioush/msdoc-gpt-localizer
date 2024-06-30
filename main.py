108f3daba5721f894c6ba3af654a80d0ebc39bff
Akira Shoji <georgioush@gmail.com>
2024-07-01 01:25:41 +0900

    altered to translate all files on a certain repo

--- main.py
+++ main.py
@@ -11,20 +11,17 @@ def main():
     repository_info_handler = RepositoryInfoHandler(repo_paths[0])
 
     # docset は複数の可能性がある (DevOps などはそう)
-    docset_handler = repository_info_handler.docsets_handlers[0]
+    for docset_handler in repository_info_handler.docsets_handlers:
+        mds_files = docset_handler.toc_handler.existing_md_files
+        folder_path = docset_handler.file_path
+        mds_paths = [os.path.join(folder_path, md) for md in mds_files]
 
-    mds_files = docset_handler.toc_handler.existing_md_files
+        print(mds_paths)
 
-    folder_path = docset_handler.file_path
-
-    mds_paths = [os.path.join(folder_path, md) for md in mds_files]
-
-    print(mds_paths)
-
-    for md_path in mds_paths:
-        markdown_handler = MarkdownTranslator(md_path)
-        markdown_handler.translate()
-        markdown_handler.save_translation()
+        for md_path in mds_paths:
+            markdown_handler = MarkdownTranslator(md_path)
+            markdown_handler.translate()
+            markdown_handler.save_translation()
 
 if __name__ == "__main__":
     main()
